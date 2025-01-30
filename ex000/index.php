7ed208e80bf890994aa5fba73d29217898ebbf86
diegorezm <dmourarezende@gmail.com>
2025-01-30 19:32:47 -0300

    wip

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>
+    <?php
+    echo "a";
+    ?>
+    <h1>omg that a</h1>
+  </h1>
+</body>
+
+</html>
